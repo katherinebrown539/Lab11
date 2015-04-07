f6eb77a48ba3c950fa6a777374be9afd75214c6e
Katherine Brown <Katherine Brown>
2015-04-06 21:09:51 -0500

    Done

--- DrawBST.cpp
+++ DrawBST.cpp
@@ -1,3 +1,9 @@
+/*
+	Lab11
+	CSC 2111 - Lab 11 Binary Search Tree
+	Katherine (Katie) Brown (worked alone due to being absent) 
+	https://github.com/kebrown46/Lab11
+*/
 #include <gtkmm.h>
 #include "BinarySearchTree.h"
 #include "BinaryTreeIterator.h"
--- TreeNode.h
+++ TreeNode.h
@@ -1,3 +1,9 @@
+/*
+	Lab11
+	CSC 2111 - Lab 11 Binary Search Tree
+	Katherine (Katie) Brown (worked alone due to being absent) 
+	https://github.com/kebrown46/Lab11
+*/
 #if !defined (TREENODE_H)
 #define TREENODE_H
 
